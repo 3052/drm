5f02c73fa9715613df07e154681f8d65cbb560d2
27 <27@riseup.net>
2026-04-02 15:46:57 -0500

    modified:   research/playReady/chain_test.go

--- research/playReady/chain_test.go
+++ research/playReady/chain_test.go
@@ -4,7 +4,6 @@ import (
    "bytes"
    "encoding/base64"
    "encoding/json"
-   "fmt"
    "io"
    "net/http"
    "os"
@@ -52,7 +51,6 @@ func TestCrave(t *testing.T) {
    if err != nil {
       t.Fatal(err)
    }
-   fmt.Println(string(payload))
    data, err = json.Marshal(map[string]any{
       "payload": payload,
       "playbackContext": map[string]any{
@@ -60,7 +58,7 @@ func TestCrave(t *testing.T) {
          "contentpackageId": 8401705,
          "destinationId": 1880,
          "platformId": 1,
-  "jwt": "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJzdWIiOiI2OTk2N2RhOWM5M2VlZjVkZjIwZjg3MTIiLCJzY29wZSI6ImFjY291bnQ6d3JpdGUgZGVmYXVsdCBtYXR1cml0eTphZHVsdCIsImlzcyI6Imh0dHBzOi8vYWNjb3VudC5iZWxsbWVkaWEuY2EiLCJjb250ZXh0Ijp7InByb2ZpbGVfaWQiOiI2OTk3MGVmYTczMTc2ZDJiMmU1M2E1YTMiLCJicmFuZF9pZHMiOlsiMWQ3MmQ5OTBjYjc2NWRlN2U0MjExMTExIiwiMWQ3MmQ5OTBjYjc2NWRlN2U0MjExMTE0IiwiMWQ3MmQ5OTBjYjc2NWRlN2U0MjExMTE1Il19LCJleHAiOjE3NzUxMTg4NzksImlhdCI6MTc3NTEwNDQ3OSwidmVyc2lvbiI6IlYyIiwianRpIjoiODgyZDBlMjEtY2NkMS00YzM3LWEyODItZjNiOTljOWI0ZjNhIiwiYXV0aG9yaXRpZXMiOlsiUkVHVUxBUl9VU0VSIl0sImNsaWVudF9pZCI6ImNyYXZlLXdlYiJ9.V-o3nqFcJQVo55gVUM2NTPA2d-xVZfCZ4r3XTUuZMDNgntNGECwpMfEBP6GiiXaj3lLq1tw8sXdzAnzx513z-Vy14uDm0lkSO8c-dHU96mpsArwCBT4agu8CG7AkPzA33WBInC4pzH4oZBCOr2TqOiM92o3TUqeSuPgTzK9N234wYC7WBWmLqzckAUtxYo5zacbdLbk8JVtWcU3TgEvBiYwP-67oddHi2nWA0bTvmqV3oW98ljjG46eYZgn8ArV83jkkT9sH7gzHUh6RzazW_ietTi6k1yzogeaxBYrpcHmMv0S1z8QgFIotqFlii7r0Jr_i-mDknBCS_bzb-zAvhA",
+         "jwt": "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJzdWIiOiI2OTk2N2RhOWM5M2VlZjVkZjIwZjg3MTIiLCJzY29wZSI6ImFjY291bnQ6d3JpdGUgZGVmYXVsdCBtYXR1cml0eTphZHVsdCIsImlzcyI6Imh0dHBzOi8vYWNjb3VudC5iZWxsbWVkaWEuY2EiLCJjb250ZXh0Ijp7InByb2ZpbGVfaWQiOiI2OTk3MGVmYTczMTc2ZDJiMmU1M2E1YTMiLCJicmFuZF9pZHMiOlsiMWQ3MmQ5OTBjYjc2NWRlN2U0MjExMTExIiwiMWQ3MmQ5OTBjYjc2NWRlN2U0MjExMTE0IiwiMWQ3MmQ5OTBjYjc2NWRlN2U0MjExMTE1Il19LCJleHAiOjE3NzUxNzcwNTEsImlhdCI6MTc3NTE2MjY1MSwidmVyc2lvbiI6IlYyIiwianRpIjoiMTRmYzI5OTktOGE5Yy00MGM3LWJhZmQtZjk0NTgwMTY4OGViIiwiYXV0aG9yaXRpZXMiOlsiUkVHVUxBUl9VU0VSIl0sImNsaWVudF9pZCI6ImNyYXZlLXdlYiJ9.mAsFKgZ0ez7LAYD30E0okAryrcVMtE0RvdjXS9i7j6wHY6B_w3snV-_kE5r-x0yvvQISaVBYmbUSkrSKzq_XEElZCWzJKyGzz28eyRkQJ-Jx2sigEmuA-vyLRaqcz3bK09fKbg4c1ekIv9uOTV2tJqnbP4cXMu7Cazp_thhQ3HtXyA9rzDm4vhoyhTkoo0mTKs--uhpxgO03UbuR7zPWbbAQsvy0yWCjgaEf61xKG6G9j6qp95g5cbt43UYv0OMzKZRAdu_61r8nGG3eTiqP_nyUu1fVDAm9Eb8AqHxdz7CnFcWyMEysMdAKD6NneukC9cLW2LtjBWYw0XaSLsBu9w",
       },
    })
    if err != nil {
